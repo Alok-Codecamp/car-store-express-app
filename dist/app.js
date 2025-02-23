e012c0f04effc7170654369e5040ba794374df92
Alok-Codecamp <alok61.bd@gmail.com>
2025-02-23 18:18:38 +0600

    change cors origin

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 //  middleware setup
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: 'https://motion-era.vercel.app/', credentials: true }));
+app.use((0, cors_1.default)({ origin: 'https://motion-era.vercel.app', credentials: true }));
 app.use((0, cookie_parser_1.default)());
 // base URL path for carRoutes module
 app.use('/api', routes_1.default);
